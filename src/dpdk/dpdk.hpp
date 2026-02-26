3dc2102656c6eab179bc68bef655dae4f4a68fa1
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-02-26 04:31:29 +0000

    push data into dpdk write buffer for writing

--- src/dpdk/dpdk.hpp
+++ src/dpdk/dpdk.hpp
@@ -1,10 +1,17 @@
+#pragma once
+
 #include <stop_token>
+#include <vector>
 
 namespace netbook::dpdk {
+
+using DataCallbackSignature = void(*)(char*, size_t);
 
 bool initialise();
 void cleanup();
 void poll_write(std::stop_token stop);
 void poll_read(std::stop_token stop);
+void push_data(char* data, size_t data_length);
+void register_receiver(DataCallbackSignature callback);
 
 }
