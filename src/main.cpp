972ecf81b1405daa32097810e106f190de4d739d
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-02-28 10:57:46 +0000

    make main loop sleep for 200ms instead of 100ms

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ void poll() {
             break;
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
     print_thread.request_stop();
