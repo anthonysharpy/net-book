20bff9206e598a012184e45774ca29b843378c63
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-03-31 02:09:26 +0100

    increase mempool size

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -10,5 +10,6 @@ constexpr std::uint32_t dpdk_queue_count = 6;
 constexpr std::uint64_t packet_limit = 1000000;
 constexpr std::uint64_t packets_per_queue = packet_limit / dpdk_queue_count;
 constexpr std::uint32_t packet_batch_size = 32;
+constexpr std::uint32_t mempool_size = 8191;
 
 }
