9c60574fe27248c2663fd7d2f8f9ebdfb126cc83
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-03-31 02:05:42 +0100

    remove unused const

--- src/constants/constants.hpp
+++ src/constants/constants.hpp
@@ -6,7 +6,6 @@
 
 namespace netbook::constants {
 
-constexpr std::uint64_t print_delay_ms = 500;
 constexpr std::uint32_t dpdk_queue_count = 6;
 constexpr std::uint64_t packet_limit = 1000000;
 constexpr std::uint64_t packets_per_queue = packet_limit / dpdk_queue_count;
